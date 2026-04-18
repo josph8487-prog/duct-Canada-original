8593a04cb7e65d7c60579aa04030b587ad1fdb37
josph8487-prog <josph8487@gmail.com>
2026-04-18 05:25:14 +0500

    Create page.tsx

---
+++ src/app/location/richmond-hill/page.tsx
@@ -0,0 +1,18 @@
+export default function RichmondHillPage() {
+  return (
+    <section style={{ padding: "50px" }}>
+      <h1>Air Duct Cleaning Services in Richmond Hill</h1>
+
+      <p>
+        Professional HVAC and duct cleaning services in Richmond Hill Ontario.
+        Trusted local cleaning experts.
+      </p>
+
+      <iframe
+        src="https://www.google.com/maps?q=Richmond%20Hill&output=embed"
+        width="100%"
+        height="400"
+      />
+    </section>
+  );
+}
